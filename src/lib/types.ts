87ce696bb234006f882306a9d2cad1aa82e237bd
HaoJ <110011336+Jethaoo@users.noreply.github.com>
2026-01-30 18:14:59 +0800

    Update dependencies in package.json and add optional team_colour property to Driver interface

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,6 +4,7 @@ export interface Driver {
   full_name: string;
   name_acronym: string;
   team_name: string;
+  team_colour?: string;
   headshot_url: string;
   country_code: string;
   session_key: number;
